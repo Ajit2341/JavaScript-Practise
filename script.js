a354e76398d9b9dfb2814452dad0b46fc26d4339
Raven2341 <91017920+Ajit2341@users.noreply.github.com>
2023-09-20 18:23:36 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+/*
+let js = "amazing"
+console.log(40 +8 + 23 -10);
+
+const PI = 3.142
+let nameYear = "parley"
+
+//'country', 'continent' and 'population'
+//Assingment 1
+
+let country = 'India';
+let continent = 'Asia';
+let population = 2000000000;
+let isIsland = false;
+let language;
+// let isIsland
+
+var x = 10
+
+{
+    var x = 2
+}
+
+console.log("value of x",x)
+
+let y = 10
+
+{
+    let y = 2
+}
+
+console.log("value of y",y)
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(typeof(isIsland));
+console.log(language);
+*/
+
+
+/*
+//operator
+const ageNow = 2050
+const ageAjit = ageNow -1995
+const ageShaila = ageNow -1969
+console.log("ajtt age",ageAjit, ageShaila);
+console.log("ajtt age multi divide",ageAjit *2, ageAjit /2);
+// 2 to the power of 3
+console.log(2**3);
+
+const firstName = 'Ajit'
+const lastName  ='Auti'
+console.log(firstName +' '+ lastName);
+
+// operator precedence
+// below 10+5 happens first then the value will be assigned to variables
+let h  =  10+5; // 15
+h +=10;  // h = h + 10
+h *= 4 // h = h *4
+h++
+h--
+h--
+console.log(h);
+
+console.log(ageAjit >= ageShaila);
+
+const comparAge = ageAjit <= ageShaila
+console.log(comparAge);
+
+*/
+
