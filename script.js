63a09d6bc4de3f1dcbbb6d1fb3d5e0626452ad95
Raven2341 <91017920+Ajit2341@users.noreply.github.com>
2023-09-22 22:52:23 +0530

    function declaration

--- script.js
+++ script.js
@@ -77,20 +77,103 @@ console.log(comparAge);
 // // math operator first than comparison
 // console.log(ageNow -1995 > ageNow -2018);
 
-const firstName = 'Jonas'
-const job = 'teacher'
-const birthYear = 1991
-const year = 2034
+// const firstName = 'Jonas'
+// const job = 'teacher'
+// const birthYear = 1991
+// const year = 2034
 
-const jonas = "I'm " + firstName + ',a ' +(year - birthYear) +' years old' + job + '!';
+// const jonas = "I'm " + firstName + ',a ' +(year - birthYear) +' years old' + job + '!';
 
-console.log(jonas);
+// console.log(jonas);
 
-const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job} !`
-console.log(jonasNew);
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job} !`
+// console.log(jonasNew);
 
-console.log(`hey i am ${job}`);
+// console.log(`hey i am ${job}`);
 
-console.log(`lorem6
-adfsdfs
-asdfasdf`);
+// console.log(`lorem6
+// adfsdfs
+// asdfasdf`);
+
+/*
+type conversion and coercion
+*/
+/*
+const inputYear = '1995'
+console.log(Number(inputYear), inputYear);
+
+console.log(Number(inputYear) +28);
+
+console.log(Number('ajit'));
+
+console.log(typeof(NaN));
+
+//type coersion
+
+console.log('23' - '10' -3);
+console.log('23' + '10' + 3);
+*/
+/*
+const hadDriverLicense = true;
+const hasGoodVision = true;
+
+console.log(hasGoodVision && hadDriverLicense);
+console.log(hasGoodVision || hadDriverLicense);
+
+const isTired = true
+
+const shouldDrive = hasGoodVision && hadDriverLicense;
+
+if (hasGoodVision && hadDriverLicense && !isTired){
+    console.log('Sarah should drive');
+}else{
+    console.log('Sarah should not drive');
+}
+
+const age =23
+
+const drink = age >= 18 ? 'wine' : 'water'
+
+console.log(drink)
+
+console.log(` I like to drink ${age>=18 ? 'wine' : 'water'} as a person`);
+
+*/
+/*
+function fruitProccessor (apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} orange`
+    return juice;
+}
+
+const juiceToDrink = fruitProccessor(5, 0)
+console.log(juiceToDrink);
+
+console.log(fruitProccessor(5, 0));
+
+const appleOrangjuice = fruitProccessor(5, 10)
+console.log(appleOrangjuice);
+*/
+
+
+/*
+const age1 = calcAge(1995)
+
+// const age2 = calcAge2(1995)
+
+// script.js:159 Uncaught ReferenceError: Cannot access 'calcAge2' before initialization
+// in declarative function or anonymous funtion canot be called before they are created coz of //hoisting
+// const age2 = calcAge2(1995)
+
+//function declartion
+function calcAge(birthYear){
+    return 2023 - birthYear;
+}
+
+//function expression
+const calcAge2 =function calcAge(birthYear){
+    return 2023 - birthYear;
+}
+const age2 = calcAge2(1995)
+console.log(`age from age1 function ${age1} and age2 from the anonymous funtion ${age2}`);
+*/
